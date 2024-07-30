70a64a8a755e8825b036fa2e8e0d97ee00c712f1
Angry Baker <checheninlive@mail.ru>
2024-07-30 08:48:50 +0300

    base full complete

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,19 +1,10 @@
 import healthCheker from '../app';
 
-test('should health status be healthy', () => {
-  const result = healthCheker({name: 'Маг', health: 90});
-
-  expect(result).toBe("healthy");
-});
-
-test('should health status be wounded', () => {
-  const result = healthCheker({name: 'Маг', health: 45});
-
-  expect(result).toBe("wounded");
-});
-
-test('should health status be critical', () => {
-  const result = healthCheker({name: 'Маг', health: 12});
-
-  expect(result).toBe("critical");
+test.each([
+    [ {name: 'Маг', health: 90}, "healthy"],
+    [ {name: 'Маг', health: 45}, "wounded"],
+    [ {name: 'Маг', health: 12}, "critical"],
+])("testing health status function with %o", (obj, expected) => {
+    const result = healthCheker(obj);
+    expect(result).toBe( expected);
 });
